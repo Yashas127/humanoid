ca09a48d704034f60a20dc767d5e0eeb566e5019
Yashas127 <yashasd21@iitk.ac.in>
2022-07-18 21:02:05 +0530

    commit

---
+++ src/components.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function App {
+  return (
+<div>
+  <Typography variant="h1">Hello wrld</Typography>
+</div>
+  );
+}
